053c1a0540a5025f29639ca327bedc6756f70187
patracheTest <ssk5442@chungbuk.ac.kr>
2023-04-11 17:31:17 +0900

    [chore]: project structure init

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_matrix();
+void addition_matrix();
+void subtraction_matrix();
+void transpose_matrix();
+void multiply_matrix();
+void free_matrix();
+
+int main(){
+    printf("[----- [김상수] [2018038078] -----]")
+}
+
+void print_matrix() {
+
+}
+
+void addition_matrix() {
+
+}
+
+void subtraction_matrix() {
+
+}
+
+void transpose_matrix() {
+
+}
+
+void multiply_matrix() {
+
+}
+
+void free_matrix() {
+
+}
