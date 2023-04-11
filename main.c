0e1358321428de556ac017867386abbb498ad202
patracheTest <ssk5442@chungbuk.ac.kr>
2023-04-11 19:00:31 +0900

    [feat]: matrix util function added

--- main.c
+++ main.c
@@ -1,19 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix();
+typedef struct
+{
+    int row;
+    int column;
+    int **matrixValue;
+} matrix;
+
+void test();
+void setMatrix(matrix *matrix);
+void giveMatrixSpace(matrix *matrix);
+void free_matrix(matrix *matrix);
+void print_matrix(matrix matrix);
 void addition_matrix();
 void subtraction_matrix();
 void transpose_matrix();
 void multiply_matrix();
-void free_matrix();
 
-int main(){
-    printf("[----- [김상수] [2018038078] -----]")
+int main() {
+    printf("[----- [김상수] [2018038078] -----]\n");
+    test();
 }
 
-void print_matrix() {
+void test() {
+    matrix matrixA = {2, 2};
+    matrix matrixB = {3, 3};
 
+    setMatrix(&matrixA);
+    setMatrix(&matrixB);
+    print_matrix(matrixA);
+    print_matrix(matrixB);
+    free_matrix(&matrixA);
+    free_matrix(&matrixB);
+
+}
+
+void setMatrix(matrix *matrix) {
+    giveMatrixSpace(matrix);
+    for (int i = 0; i < matrix->column; i++)
+    {
+        for (int j = 0; j < matrix->row; j++)
+        {
+            matrix->matrixValue[i][j] = i+j;
+        }
+    }
+}
+
+void giveMatrixSpace(matrix *matrix) {
+    matrix->matrixValue = (int**)malloc(sizeof(int*)*matrix->column);
+    for (int i = 0; i < matrix->column; i++)
+    {
+        *(matrix->matrixValue+i) = (int*)malloc(sizeof(int)*matrix->row);
+    }
+}
+
+void free_matrix(matrix *matrix) {
+    free(matrix->matrixValue);
+    printf("matrix free executed\n");
+}
+
+void print_matrix(matrix matrix) {
+    printf("matrix value\n");
+    for (int i = 0; i < matrix.column; i++)
+    {
+        for (int j = 0; j < matrix.row; j++)
+        {
+            printf("%d ", matrix.matrixValue[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void addition_matrix() {
@@ -29,9 +86,5 @@ void transpose_matrix() {
 }
 
 void multiply_matrix() {
-
-}
-
-void free_matrix() {
 
 }
